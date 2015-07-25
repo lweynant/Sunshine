4c475cd1340996ed76c9820cf3acb671ee1c196d
lweynant <lweynant@users.noreply.github.com>
2015-07-25 21:23:38 +0200

    prepare sync adapter

--- app/src/main/java/com/lweynant/sunshine/ForecastFragment.java
+++ app/src/main/java/com/lweynant/sunshine/ForecastFragment.java
@@ -23,6 +23,7 @@ import android.widget.ListView;
 
 import com.lweynant.sunshine.data.WeatherContract;
 import com.lweynant.sunshine.service.SunshineService;
+import com.lweynant.sunshine.sync.SunshineSyncAdapter;
 
 
 /**
@@ -181,18 +182,19 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
 
     private void updateWeatherForecast() {
         Log.d(LOG_TAG, "updateWeatherForecast");
+        SunshineSyncAdapter.syncImmediately(getActivity());
 
-        Intent alarmIntent = new Intent(getActivity(), SunshineService.AlarmReceiver.class);
-        alarmIntent.putExtra(SunshineService.LOCATION_QUERY_EXTRA, Utility.getPreferredLocation(getActivity()));
-
-        //Wrap in a pending intent which only fires once.
-        PendingIntent pi = PendingIntent.getBroadcast(getActivity(), 0,alarmIntent,PendingIntent.FLAG_ONE_SHOT);
-
-
-        AlarmManager am=(AlarmManager)getActivity().getSystemService(Context.ALARM_SERVICE);
-
-        //Set the AlarmManager to wake up the system.
-        am.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 5000, pi);
+//        Intent alarmIntent = new Intent(getActivity(), SunshineService.AlarmReceiver.class);
+//        alarmIntent.putExtra(SunshineService.LOCATION_QUERY_EXTRA, Utility.getPreferredLocation(getActivity()));
+//
+//        //Wrap in a pending intent which only fires once.
+//        PendingIntent pi = PendingIntent.getBroadcast(getActivity(), 0,alarmIntent,PendingIntent.FLAG_ONE_SHOT);
+//
+//
+//        AlarmManager am=(AlarmManager)getActivity().getSystemService(Context.ALARM_SERVICE);
+//
+//        //Set the AlarmManager to wake up the system.
+//        am.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 5000, pi);
     }
 
 
