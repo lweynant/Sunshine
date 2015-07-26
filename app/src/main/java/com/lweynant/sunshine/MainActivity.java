b3f21a287a3fcd6ef7c3a5434a6a7b245a5c7f3f
lweynant <lweynant@users.noreply.github.com>
2015-07-26 20:59:17 +0200

    scheduled synchronization

--- app/src/main/java/com/lweynant/sunshine/MainActivity.java
+++ app/src/main/java/com/lweynant/sunshine/MainActivity.java
@@ -10,6 +10,8 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.lweynant.sunshine.sync.SunshineSyncAdapter;
+
 
 public class MainActivity extends ActionBarActivity  implements ForecastFragment.Callback{
 
@@ -43,6 +45,7 @@ public class MainActivity extends ActionBarActivity  implements ForecastFragment
         }
         ForecastFragment forecastFragment = (ForecastFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_forecast);
         forecastFragment.setUseTodayLayout(!mTwoPane);
+        SunshineSyncAdapter.initializeSyncAdapter(this);
     }
 
     @Override
